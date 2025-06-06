19f50aeb3ee5a91e3faa7a086ee9aad4e307d3a4
Storge <2049216062@qq.com>
2025-06-06 16:34:41 +0800

    init commit

---
+++ src/main/java/com/storge/mycommunitydemo/MyCommunityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.storge.mycommunitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCommunityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCommunityDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/storge/mycommunitydemo/MyCommunityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storge.mycommunitydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCommunityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
